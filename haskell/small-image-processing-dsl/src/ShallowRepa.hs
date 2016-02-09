c253e8b82f7624574a00a75cf6b23133a9a52a9f
Rob Stewart <robstewart57@gmail.com>
2016-02-09 17:08:22 +0000

    (Haskell) placeholder for shallow repa embedding

---
+++ haskell/small-image-processing-dsl/src/ShallowRepa.hs
@@ -0,0 +1,16 @@
+
+module ShallowRepa where
+
+import Data.Array.Repa
+import Types
+
+run :: RepaImage -> IO RepaImageComputed
+run = undefined
+
+blurX,blurY :: RepaImage -> RepaImage
+blurX = undefined
+blurY = undefined
+
+brighten,darken :: Int -> RepaImage -> RepaImage
+brighten = undefined
+darken = undefined
--- haskell/small-image-processing-dsl/src/Types.hs
+++ haskell/small-image-processing-dsl/src/Types.hs
@@ -1,9 +1,15 @@
+{-# Language TypeOperators #-}
 
 module Types where
 
 import Data.Vector
+import Data.Array.Repa (Array,D,U,Z,(:.))
+import qualified Data.Array.Repa as R
 
 data RelativePosition = Above | Below | LeftOf | RightOf
+
+type RepaImage = Array D (Z :. Int :. Int) (Int,Int,Int)
+type RepaImageComputed = Array U (Z :. Int :. Int) (Int,Int,Int)
 
 data VectorImage = VectorImage
            { pixels :: Vector (Int,Int,Int) -- ^ row major order
